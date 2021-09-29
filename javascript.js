4d031e58b897764de1eb7b0cf593ccce1925d0a7
arthurticianeli <arthurpeixoto@gmail.com>
2021-09-29 16:40:57 -0300

    commit final para entrega

--- javascript.js
+++ javascript.js
@@ -1,33 +1,55 @@
+//*****************************************************************************/
+//****************************** VARIÁVEIS GLOBAIS ****************************/ 
+//*****************************************************************************/
 
-// Escolha da dificuldade3
-/*torre2Play.addEventListener("click", dificuldade)
-
-function dificuldade (e) {
-
-   escolha.style.display = "flex"
-}*/
-
+const containerPlay = document.querySelector("#containerPlay")
+const torre2Play = document.querySelector("#torre2Play")
+const containerRegras = document.querySelector("#regras")
+const containerJogada = document.querySelector("#containerJogada")
+const containerResultado = document.querySelector("#containerResultado")
+const escolha = document.querySelector("#escolha")
+const footer = document.querySelector("footer")
+const jogarNovamente = document.querySelector("#jogarNovamente")
+const escolher = document.querySelector("#escolherDificuldade")
+const fechar = document.querySelector("#regras span")
 const jogada = document.querySelector('#containerJogada')
-
+const showClick = document.createElement('span')
+const resetJogada = document.createElement('span')
+const voltarTelaInicial = document.createElement('span')
 let nivelDificuldade = ''
+let countClick = 0
+let disco = ''
+let torre = ''
+let mudancas = 0
 
-// essa função gera as torres e discos no HTML via DOM
-// o parametro N é passado pela função mostrarJogada, onde o usuário escolha o nível de dificuldade
+
+//*****************************************************************************/
+//****************************** REGRAS DO NEGÓCIO ****************************/ 
+//*****************************************************************************/
+
+
+// ****************** REGRA: CRIAÇÃO DAS TORRES POR DOM  *********************//
+
 function geracaoTorreHanoi(n){
 
-    // esse laço cria as 3 torres
     for(let i=0; i<3; i++){
         const torresJogo = document.createElement('div')
         torresJogo.id = `torre${i+1}`
         jogada.appendChild(torresJogo)
     }
     geracaoDisco(n)
+
 }
 
-function geracaoDisco(n){
-    const primeiraTorre = document.getElementById('torre1') // variavel encontra e armazena a primeira torre
 
-    // cada uma das condições abaixo servem para gerar uma quantidade diferente de discos, elevando a dificuldade do jogo
+// ***** REGRA: CRIAÇÃO DOS DISCOS DE ACORDO COM A DIFICULDADE ESCOLHIDA *****//
+
+function geracaoDisco(n){
+
+    makeFooter(mudancas)
+
+    const primeiraTorre = document.getElementById('torre1')
+
     if(n === 'dificuldade1'){
 
         for(let i=0; i<3; i++){
@@ -53,149 +75,7 @@ function geracaoDisco(n){
     }
 }
 
-
-
-jogada.addEventListener('click', selectTower)
-
-
-let countClick = 0
-let disco = ''
-let torre = ''
-let mudancas = 0
-//console.log(qntsClicks)
-
-const showClik = document.createElement('span')
-
-// a função é responsável atualizar a quantidade de trocas realizadas
-// precisa confirmar o local do mostrador de contagem
-function atualizaClick (mudancas){
-
-    showClik.innerText = "Quantidade de movimentos: " + mudancas
-
-    document.querySelector("footer").appendChild(showClik)
-
-}
-
-function selectTower(e) {
-    
-    // a condição abaixo valida se o click foi realizado nas "hastes" da torre
-    if(e.target.tagName === 'DIV'){
-
-        torre = e.target.id 
-        
-        // valida a condição de primeiro click
-        if(countClick === 0){
-           
-            disco = selectDisc(torre) // devolve o id do último 'filho' da torre
-            
-            // a condição abaixo verifica se o primeiro click foi dado numa torre vazia e caso verdadeiro ele não permite que o click avance para o segundo click
-            if(disco !== null){
-                countClick++ // incremento para validação do segundo click
-            }
-            
-            // se disco null então countclick igual a zero
-    
-        } else if(countClick === 1){
-
-       
-            changeTower(torre,disco) // chama a função que irá movimentar os discos
-            countClick = 0 // zera o incremento, voltando a ser o 'primero click'
-
-
-        }
-
-        win()
-    }
-
-    // ao fim de cada jogada, a função verifica se há a condição de vitória
-    //win()
-}
-
-// função dedicada a troca dos discos entre as torres
-function changeTower(tower, disc){
-
-    const tagTower = document.getElementById(tower) // traz o elemento html específico que irá receber o disco
-    const tagDisc = document.getElementById(disc) // traz o elemento html específico que será movimentado
-
-    const DiscTowerChange = document.getElementById(selectDisc(tower)) // busca na torre do 'segundo click' o ultimo elemento para validação da regra de negócio
-
-
-    // *** validação das regras de negócio ***
-
-    // a condição abaixo verifica se a torre do segundo clique é vazia e permite a troca
-    if(selectDisc(tower) === null){
-
-        tagTower.appendChild(tagDisc)
-
-        mudancas++ //#Bonus = implenta a contagem de clicks no jogo. No botão reset, zerar a contagem.
-        
-        atualizaClick(mudancas) // chama a função que exibe na tela a contagem das trocas
-
-    // a condição abaixo compara os tamanhos do ultimo filho de cada torre e é executada quando o disco do primero click por menor que o disco existe na torre do segundo click
-    }else if(tagDisc.clientWidth < DiscTowerChange.clientWidth){
-
-        tagTower.appendChild(tagDisc)
-
-        mudancas++ //#Bonus = implenta a contagem de clicks no jogo. No botão reset, zerar a contagem.
-        
-
-        atualizaClick(mudancas) // chama a função que exibe na tela a contagem das trocas
-    }
-
-}
-
-// função dedicada a retornar o ID do ultimo filho da torre clicada
-function selectDisc(id){
-    const torreClicada = document.querySelector(`#${id}`)
-    const disco = torreClicada.lastElementChild
-
-    // caso a torre clicada não tenha discos, ele retorna null
-    if(disco === null){
-        return disco
-    }
-
-    return disco.id
-
-}
-
-
-
-
-// ***************** VARIÁVEIS ***************//
-
-const containerPlay = document.querySelector("#containerPlay")
-const torre2Play = document.querySelector("#torre2Play")
-const containerRegras = document.querySelector("#regras")
-const containerJogada = document.querySelector("#containerJogada")
-const containerResultado = document.querySelector("#containerResultado")
-const escolha = document.querySelector("#escolha")
-const rodape = document.querySelector("footer")
-
-
-const jogarNovamente = document.querySelector("#jogarNovamente")
-const escolher = document.querySelector("#escolherDificuldade")
-const fechar = document.querySelector("#regras span")
-
-
-// ***************** PLAY BUTTON ***************//
-
-// Escolha da dificuldade
-torre2Play.addEventListener("click", dificuldade)
-
-function dificuldade (e) {
-    
-    escolha.style.display = "flex"
-
-    //console.log(e.target)
-
-//  CHAMAR FUNÇÃO QUE CRIA OS DISCOS
-    
-}
-
-// ***************** ESCOLHAS ***************//
-
-escolha.addEventListener("click", mostrarJogada)
-
+// ***************** REGRA: PASSA A DIFICULDADE ESCOLHIDA ******************//
 
 function mostrarJogada (e){
 
@@ -208,20 +88,110 @@ function mostrarJogada (e){
 
     return geracaoTorreHanoi(nivelDificuldade)
 
+}
+
+// ** REGRA: CRIAÇÃO POR DOM DO CONTADOR DE MOVIMENTOS / RESET / ESCOLHER NOVA DIFICULDADE **//
+
+function makeFooter(mudancas) {
+
+    showClick.innerText = "Quantidade de movimentos: " + mudancas
+    showClick.id = "contador"
+
+    resetJogada.innerText = "Clique aqui para recomeçar"
+    resetJogada.id ="resetButton"
+    resetJogada.classList.add("effectButton")
+    
+    voltarTelaInicial.innerText = "Voltar para a tela inicial"
+    voltarTelaInicial.id ="voltarTelaInicial"
+    voltarTelaInicial.classList.add("effectButton")
+
+    footer.appendChild(voltarTelaInicial)
+    footer.appendChild(resetJogada)
+    footer.appendChild(showClick)
 
 }
 
-// ***************** FECHAR REGRAS ***************//
+//****************** REGRA: ATUALIZAÇÃO DO CONTADOR DE MOVIMENTOS *******************//
 
-fechar.addEventListener("click", close)
+function atualizaClick(mudancas) {
 
-function close() {
-     containerRegras.style.display = "none"
+    showClick.innerText = "Quantidade de movimentos: " + mudancas
+
+    document.querySelector("footer").appendChild(showClick)
+
+}
+
+//***** REGRA: TARGET DO CLICK NA TORRE E VERIFICAÇÃO DA CONDIÇÃO DE VITÓRIA *****//
+
+function selectTower(e) {
+    
+    if(e.target.tagName === 'DIV'){
+
+        torre = e.target.id 
+
+        if(countClick === 0){
+           
+            disco = selectDisc(torre) 
+            
+            if(disco !== null){
+                countClick++
+            }
+    
+        } else if(countClick === 1){
+       
+            changeTower(torre,disco)
+            countClick = 0
+
+        }
+
+        win()
+    }
+
+}
+
+//********************* REGRA: SELECIONAR O DISCO PELO LASTCHILD **********************//
+
+function selectDisc(id){
+
+    const torreClicada = document.querySelector(`#${id}`)
+    const disco = torreClicada.lastElementChild
+
+    if(disco === null){
+        return disco
+    }
+
+    return disco.id
+
+}
+
+//************************* REGRA: MUDANÇA DOS DISCOS ****************************//
+
+function changeTower(tower, disc){
+
+    const tagTower = document.getElementById(tower)
+    const tagDisc = document.getElementById(disc)
+
+    const DiscTowerChange = document.getElementById(selectDisc(tower)) // busca na torre do 'segundo click' o ultimo elemento para validação da regra de negócio
+
+    if (selectDisc(tower) === null){
+
+        tagTower.appendChild(tagDisc)
+
+        mudancas++ 
+
+    } else if (tagDisc.clientWidth < DiscTowerChange.clientWidth){
+
+        tagTower.appendChild(tagDisc)
+
+        mudancas++
+    }
+
+    atualizaClick(mudancas)
 
 }
 
 
-// ***************** CONDIÇÃO DE VITÓRIA ***************//
+// ***************** REGRA: CONDIÇÃO DE VITÓRIA ***************//
 
 function win(){
     const torre1 = document.querySelector("#torre1")
@@ -232,16 +202,81 @@ function win(){
     }
 }
 
-// ***************** JOGAR DE NOVO ***************//
 
-jogarNovamente.addEventListener("click", reset) // construtor da torre
+
+//*****************************************************************************************/
+//************************************* LISTENERS *****************************************/ 
+//*****************************************************************************************/
+
+// ***************** LISTENER: CLICK INICIAL ***************//
+
+torre2Play.addEventListener("click", dificuldade)
+
+
+function dificuldade (e) {
+    
+    escolha.style.display = "flex"
+     
+}
+
+// ***************** LISTENER: ESCOLHA DE DIFICULDADE ***************//
+
+escolha.addEventListener("click", mostrarJogada)
+
+
+
+// ***************** LISTENER: FECHAR REGRAS ***************//
+
+fechar.addEventListener("click", close)
+
+function close() {
+     containerRegras.style.display = "none"
+}
+
+// ***************** LISTENER: JOGADA ***************//
+
+jogada.addEventListener('click', selectTower)
+
+
+//*****************************************************************************************/
+//****************************************** RESETS ***************************************/ 
+//*****************************************************************************************/
+
+
+// ***************** RESET: RESETAR A PARTIDA ***************//
+
+footer.addEventListener("click", resetNow)
+
+function resetNow(e) {
+
+    if (e.target.id === "resetButton") {
+
+    const torre3 = document.querySelector("#torre3")
+
+    torre1.innerHTML = ""
+    torre2.innerHTML = ""
+    torre3.innerHTML = ""
+
+    footer.innerHTML = ""
+    mudancas = 0
+
+    geracaoDisco(nivelDificuldade)
+
+    containerResultado.style.display = "none"
+    }
+
+}
+
+// ***************** RESET: JOGAR DE NOVO A MESMA DIFICULDADE ***************//
+
+jogarNovamente.addEventListener("click", reset)
 
 function reset() {
 
     const torre3 = document.querySelector("#torre3")
 
     torre3.innerHTML = ""
-    rodape.innerHTML = ""
+    footer.innerHTML = ""
     mudancas = 0
 
     geracaoDisco(nivelDificuldade)
@@ -250,16 +285,16 @@ function reset() {
 
 }
 
+// ***************** RESET: VOLTAR PARA A TELA INICIAL ***************//
 
-// ***************** ESCOLHER DIFICULDADE ***************//
-
+voltarTelaInicial.addEventListener("click", escolherDificuldade)
 escolher.addEventListener("click", escolherDificuldade)
 
 function escolherDificuldade() {
     const torre3 = document.querySelector("#torre3")
 
     torre3.innerHTML = ""
-    rodape.innerHTML = ""
+    footer.innerHTML = ""
     mudancas = 0
     containerJogada.innerHTML = ''
 
